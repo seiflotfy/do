f61700ef0fbfd97c19cba80bcddaf22f2f0ec833
Seif Lotfy <seif@lotfy.com>
2015-05-24 17:59:35 +0200

    Add new MapParallel function

--- map.go
+++ map.go
@@ -1,6 +1,9 @@
 package gogo
 
-import "reflect"
+import (
+	"reflect"
+	"sync"
+)
 
 /*
 Map ...
@@ -15,6 +18,35 @@ func Map(function interface{}, iterable interface{}) interface{} {
 		funcInput[0] = iterValue.Index(i)
 		res := funcValue.Call(funcInput)
 		returnValue.Index(i).Set(res[0])
+	}
+	return returnValue.Interface()
+}
+
+/*
+MapParallel ...
+*/
+func MapParallel(function interface{}, iterable interface{}, numParallel int) interface{} {
+	iterValue := getIterable(iterable)
+	funcValue := getFunction(function, iterValue.Type().Elem(), nil, 1)
+	returnValue := reflect.MakeSlice(reflect.SliceOf(funcValue.Type().Out(0)), iterValue.Len(), iterValue.Len())
+	funcInput := make([]reflect.Value, 1)
+
+	var wg sync.WaitGroup
+	p := 0
+
+	defer wg.Wait()
+	for i := 0; i < iterValue.Len(); i++ {
+		wg.Add(1)
+		go func(j int) {
+			defer wg.Done()
+			funcInput[0] = iterValue.Index(j)
+			res := funcValue.Call(funcInput)
+			returnValue.Index(j).Set(res[0])
+		}(i)
+		p++
+		if p%numParallel == 0 {
+			wg.Wait()
+		}
 	}
 	return returnValue.Interface()
 }
--- map_test.go
+++ map_test.go
@@ -12,3 +12,11 @@ func TestMap(t *testing.T) {
 		t.Error("Error expected", []bool{false, true, false, true}, "got", values)
 	}
 }
+
+func TestMapParallel(t *testing.T) {
+	iterable := []int{9, 8, 7, 6, 5, 6, 6, 1}
+	values := MapParallel(func(v int) bool { return v%2 == 0 }, iterable, 2)
+	if !reflect.DeepEqual(values, []bool{false, true, false, true, false, true, true, false}) {
+		t.Error("Error expected", []bool{false, true, false, true, false, true, true, false}, "got", values)
+	}
+}
