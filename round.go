aecd8373767c4a68235c2639c4400b17db002e19
Seif Lotfy <seif@lotfy.com>
2015-05-24 23:08:50 +0200

    Add proper doc.go file

--- round.go
+++ round.go
@@ -9,7 +9,7 @@ Round returns value rounded on the decimal value.
 */
 func Round(value float64, decimal int) float64 {
 	var rounding float64
-	pow := math.Pow(10, float64(comma))
+	pow := math.Pow(10, float64(decimal))
 
 	afterComma := value * pow
 	_, fraction := math.Modf(afterComma)
